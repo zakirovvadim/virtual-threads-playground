0aa038bbb6ec3d97f931c8b19e3b5ba9d9e8cb73
VadimZakirov <appolon17@gmail.com>
2025-03-02 14:21:39 +0500

    sec 01

---
+++ src/main/java/ru/vadim/Main.java
@@ -0,0 +1,7 @@
+package ru.vadim;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ src/main/java/ru/vadim/sec01/Task.java
@@ -0,0 +1,19 @@
+package ru.vadim.sec01;
+
+import org.slf4j.Logger;
+
+import java.time.Duration;
+
+public class Task {
+    public static final Logger log = org.slf4j.LoggerFactory.getLogger(Task.class);
+
+    public static void ioIntensive(int i) {
+        try {
+            log.info("starting I|O task {}", i);
+            Thread.sleep(Duration.ofSeconds(10));
+            log.info("ending I|O task {}", i);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
